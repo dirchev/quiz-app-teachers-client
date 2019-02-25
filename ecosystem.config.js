88a3079cd9d31b3469bd678bcf4fe55a35cdb4a0
Dimitar Mirchev <dirchev@camplight.net>
2019-02-25 19:05:52 +0000

    prepare for deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [{
+    name: "main",
+    script: "index.js",
+    merge_logs: true
+  }],
+  deploy: {
+    // VMWARE local machine
+    staging: {
+      // SSH key path, default to $HOME/.ssh
+      key: "$HOME/.ssh",
+      // SSH user
+      user: "dirchev",
+      // SSH host
+      host: ["quiz-app.loc"],
+      // ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      // GIT remote
+      repo: "git@github.com:dirchev/quiz-app-teachers-client.git",
+      // path in the server
+      path: "/home/dirchev/quiz-app-teachers-client",
+      // Pre-setup command or path to a script on your local machine
+      "pre-setup": "apt-get install git ; ls -la",
+      // Post-setup commands or path to a script on the host machine
+      // eg: placing configurations in the shared dir etc
+      "post-setup": "ls -la",
+      // post-deploy action
+      "post-deploy": "npm install --production && npm run build:staging",
+    },
+  }
+}
