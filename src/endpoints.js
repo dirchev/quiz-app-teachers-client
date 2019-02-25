d87233bf8c0b1ae3a227c4e2afbe86f1da2dfd39
Dimitar Mirchev <dirchev@camplight.net>
2019-02-25 19:43:50 +0000

    fix endpoints

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,25 +1,25 @@
-const hostname = `http://localhost:8080` // TODO get hostname from env variable
+let root = process.env.REACT_APP_ROOT_URL || ''
 
-export let login = `${hostname}/api/login`
-export let register = `${hostname}/api/register`
+export let login = `${root}/api/login`
+export let register = `${root}/api/register`
 
 export let quizApps = {
-  list: `${hostname}/api/quiz-app`,
-  create: `${hostname}/api/quiz-app`,
-  update: ({quizAppId}) => `${hostname}/api/quiz-app/${quizAppId}`,
-  delete: ({quizAppId}) => `${hostname}/api/quiz-app/${quizAppId}`,
+  list: `${root}/api/quiz-app`,
+  create: `${root}/api/quiz-app`,
+  update: ({quizAppId}) => `${root}/api/quiz-app/${quizAppId}`,
+  delete: ({quizAppId}) => `${root}/api/quiz-app/${quizAppId}`,
 }
 
 export let quizess = {
-  create: ({quizAppId}) => `${hostname}/api/quiz-app/${quizAppId}/quizess`,
-  list: ({quizAppId}) => `${hostname}/api/quiz-app/${quizAppId}/quizess`,
-  update: ({quizAppId, quizId}) => `${hostname}/api/quiz-app/${quizAppId}/quizess/${quizId}`,
-  delete: ({quizAppId, quizId}) => `${hostname}/api/quiz-app/${quizAppId}/quizess/${quizId}`,
-  publish: ({quizAppId, quizId}) => `${hostname}/api/quiz-app/${quizAppId}/quizess/${quizId}/publish`
+  create: ({quizAppId}) => `${root}/api/quiz-app/${quizAppId}/quizess`,
+  list: ({quizAppId}) => `${root}/api/quiz-app/${quizAppId}/quizess`,
+  update: ({quizAppId, quizId}) => `${root}/api/quiz-app/${quizAppId}/quizess/${quizId}`,
+  delete: ({quizAppId, quizId}) => `${root}/api/quiz-app/${quizAppId}/quizess/${quizId}`,
+  publish: ({quizAppId, quizId}) => `${root}/api/quiz-app/${quizAppId}/quizess/${quizId}/publish`
 }
 
 export let teachers = {
-  list: ({quizAppId}) => `${hostname}/api/quiz-app/${quizAppId}/teachers`,
-  add: ({quizAppId}) => `${hostname}/api/quiz-app/${quizAppId}/teachers`,
-  delete: ({quizAppId, teacherId}) => `${hostname}/api/quiz-app/${quizAppId}/teachers/${teacherId}`,
+  list: ({quizAppId}) => `${root}/api/quiz-app/${quizAppId}/teachers`,
+  add: ({quizAppId}) => `${root}/api/quiz-app/${quizAppId}/teachers`,
+  delete: ({quizAppId, teacherId}) => `${root}/api/quiz-app/${quizAppId}/teachers/${teacherId}`,
 }
