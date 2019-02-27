b70eaff623ab740510cd9cc8699c7ab5eda26ee6
Dimitar Mirchev <dirchev@camplight.net>
2019-02-27 19:47:13 +0000

    add prod config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,5 +25,24 @@ module.exports = {
       // post-deploy action
       "post-deploy": "npm install --production && npm run build:staging",
     },
+    production: {
+      // SSH key path, default to $HOME/.ssh
+      key: "$HOME/.ssh",
+      // SSH user
+      user: "dirchev",
+      // SSH host
+      host: ["quiz-app.dirchev.me"],
+      // ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      // GIT remote
+      repo: "git@github.com:dirchev/quiz-app-teachers-client.git",
+      // path in the server
+      path: "/home/dirchev/quiz-app-teachers-client",
+      // Post-setup commands or path to a script on the host machine
+      // eg: placing configurations in the shared dir etc
+      "post-setup": "ls -la",
+      // post-deploy action
+      "post-deploy": "npm install --production && npm run build:staging",
+    },
   }
 }
