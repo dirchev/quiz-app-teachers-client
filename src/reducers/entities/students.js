21ea0399db6ee890920ccf956f9b5237a6f8e316
Dimitar Mirchev <dirchev@camplight.net>
2019-02-28 18:29:02 +0000

    add handlers for students related actions

--- src/reducers/entities/students.js
+++ src/reducers/entities/students.js
@@ -1,11 +1,16 @@
-const DEFAULT_STATE = {
-  'S111111111111': { id: 'S111111111111', name: 'Dimitar Mirchev', email: 'dimitar.mirchev96@gmail.com' },
-  'S222222222222': { id: 'S222222222222', name: 'Samuel Jackson', email: 'sam.jack@gmail.com' },
-  'S333333333333': { id: 'S333333333333', name: 'Peter Pan', email: 'peter.pan@gmail.com' },
-  'S444444444444': { id: 'S444444444444', name: 'Juliet McDonalds', email: 'jul@gmail.com' }
-}
-const studentsEntities = (state = DEFAULT_STATE, action) => {
-  return state
+import arrayToEntities from "utils/arrayToEntities";
+
+const DEFAULT_STATE = {}
+const teachersEntities = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+    case 'STUDENTS_LIST_SUCCESS':
+      return {
+        ...state,
+        ...arrayToEntities(action.payload.students)
+      }
+    default:
+      return state
+  }
 }
 
-export default studentsEntities
+export default teachersEntities
