1583f499fd0c3ed793dbcccffeb99bfaa56aff5c
Dimitar Mirchev <dirchev@camplight.net>
2019-03-27 20:06:58 +0000

    Allow correct answer to be specified for short text quiz

--- src/pages/QuizApp/Quiz/QuizForm/QuestionForm/AnswersForm/FreeShortText.js
+++ src/pages/QuizApp/Quiz/QuizForm/QuestionForm/AnswersForm/FreeShortText.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react"
+import Input from 'components/Form/Input'
 
 class FreeShortText extends Component {
+  constructor () {
+    super()
+    this.handleCorrectAnswerChange = this.handleCorrectAnswerChange.bind(this)
+  }
+
+  handleCorrectAnswerChange (event) {
+    let value = event.target.value
+    this.emitAnswersChange({
+      correctAnswer: value
+    })
+  }
+
+  emitAnswersChange (updatedAnswers) {
+    this.props.onAnswersChange(updatedAnswers)
+  }
   render () {
     return (
       <div>
@@ -10,9 +26,21 @@ class FreeShortText extends Component {
         <div className="alert alert-info">
           Students will be given a one line text input to enter their answer.
         </div>
+        <Input
+          label="Correct answer"
+          placeholder="Put the correct answer here."
+          type="text"
+          value={this.props.answers.correctAnswer}
+          onChange={this.handleCorrectAnswerChange}
+        />
       </div>
     )
   }
 }
 
+FreeShortText.defaultProps = {
+  answers: {
+    correctAnswer: ''
+  }
+}
 export default FreeShortText
